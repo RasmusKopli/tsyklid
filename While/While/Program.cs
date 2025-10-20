440239ac5f6c4e6931e577acf3e531c11581bb84
Opilane TTHK <opilane@tthk.ee>
2025-10-20 10:20:16 +0300

    while tsükli näidis 2 koos case näidisega

--- While/While/Program.cs
+++ While/While/Program.cs
@@ -14,3 +14,63 @@
 //    }
 //    i++;
 //}
+
+Console.WriteLine("Tere tulemast MobiFix parandusautomaati! Kuidas saame aidata?");
+Console.WriteLine("0- headaega\n1 - tahan telefoni parandada.");
+int kasutajaValik = int.Parse(Console.ReadLine());
+while(kasutajaValik < 0 && kasutajaValik > 1)
+{
+    Console.WriteLine("Palun tee oma valik kirjutades vastav arv");
+    kasutajaValik = int.Parse(Console.ReadLine());
+}
+if (kasutajaValik == 0)
+{
+    Console.WriteLine("Headaega, tulge jälle!");
+}
+else
+{
+    Console.WriteLine("Palun sisesta oma telefoni mudel, mida parandada soovid; ");
+    Console.WriteLine("1-iPhone\n2-Xiaomi\n-Huawei\n4-Samsung");
+    int kasutajaTelefon = int.Parse(Console.ReadLine());
+    while(kasutajaTelefon < 1 && kasutajaTelefon > 4)
+    {
+        kasutajaTelefon = int.Parse(Console.ReadLine());
+        Console.WriteLine("Palun tee oma valilk, kirjutades vastav arv, ");
+    }
+    switch (kasutajaTelefon)
+    {
+        case 1:
+            Console.WriteLine("Aitäh, oma Iphone saad tagasi 1 nädala pärast");
+            break;
+    }
+    switch (kasutajaTelefon)
+    {
+        case 2:
+            Console.WriteLine("Aitäh, oma Xiaomi saad tagasi 1 nädala pärast");
+            break;
+    }
+    switch (kasutajaTelefon)
+    {
+        case 3:
+            Console.WriteLine("Aitäh, oma Huawei saad tagasi 1 nädala pärast");
+            break;
+    }
+    switch (kasutajaTelefon)
+    {
+        case 4:
+            Console.WriteLine("Aitäh, oma Samsungi saad tagasi 2 päeva pärast");
+            break;
+    }
+    Console.WriteLine("Kas soovid mõnda muud mudelit paradada?");
+    Console.WriteLine("0 - Jah\n1 - ei");
+    int kasutajateinevalik = 0;
+    while (kasutajateinevalik < 0 && kasutajateinevalik > 1)
+    {
+        Console.WriteLine("Palun tee oma valik kirjutades vastav arv");
+        kasutajaValik = int.Parse(Console.ReadLine());
+    }
+    if (kasutajateinevalik == 0)
+    {
+        kasutajaTelefon = kasutajateinevalik;
+    }
+}
