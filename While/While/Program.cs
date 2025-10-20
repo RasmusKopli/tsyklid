e85222b468845d8f03607f7510d26f45c128be6d
Opilane TTHK <opilane@tthk.ee>
2025-10-20 09:12:25 +0300

    esimene while näide
    koos vooskeemiga

---
+++ While/While/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Joonistan sulle kolmnurga:");
+int kolmnurgasuurus = int.Parse(Console.ReadLine());
+
+int i = 1;
+while(i <= kolmnurgasuurus)
+{
+    Console.WriteLine();
+    int j = 1;
+    while(j <= i)
+    {
+        Console.Write(j + " ");
+        j++;
+    }
+    i++;
+}
