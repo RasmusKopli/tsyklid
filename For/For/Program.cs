412ee858c2b7c841226dcd679ef38ab3867e66fe
Opilane TTHK <opilane@tthk.ee>
2025-10-27 09:55:58 +0200

    For näited

---
+++ For/For/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("For tsükkel");
+
+//int kogus = 5;
+
+//for (int i = 0; i < kogus; i++);
+//{
+//    Console.WriteLine("E");
+//    Console.Beep();
+//}
+
+//string[] objectArray = new string[5]; //array, massiiv mitmest elemendist
+//int objectCount = 0; //loend, kui palju kordi küsida
+//string temp = "No name"; //muutuja mis hetkel on "tühi"
+//while (objectCount < 5) //while tsükkel mis küsib nimesid
+//{
+//    Console.WriteLine("Sisesta järgmine lemmikloomanimi");
+//    temp = Console.ReadLine(); //temp muutujasse väärtuse (mida me tahame) sisestamine
+//    objectArray[objectCount] = (temp); //massivi, asukohal objectcount asetame ajutisest muutujast väärtuse
+//    objectCount++; //tsükli inkrementatsioon
+//}
+//Console.WriteLine("Oled sisestanud järgnevad objektid"); //sõnum
+//for (int i = 0; i < objectArray.Length; i++)
+//{
+//    Console.WriteLine($"{i+1}. Element on: {objectArray[i]}");
+//    //kuvame välja reanumbri koos vastava elemendiga, muutuja i abil
+//}
+//Console.ReadLine(); //peatame programmi klahvisisestuse taha
